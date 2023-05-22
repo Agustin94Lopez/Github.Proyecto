1ddac854836ba30b518e8966ee048639e0cc0098
Agust <agustinelectronica@hotmail.com>
2023-05-22 20:11:10 +0200

    primer commit

---
+++ src/df.java
@@ -0,0 +1,11 @@
+public class df {
+    public static void main(String[] args) {
+        System.out.println("Hola! este proyecto es para decir: ");
+        int numero=8;
+
+        for(int i=0; i<numero; i++){
+            System.out.println("Aleix");
+        }
+    }
+}
+
